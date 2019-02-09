2de2fd977269fdac4800f29205ceed685d797d7e
Michal Pasierbski <michal.pasierbski@r3pi.io>
2019-02-09 14:01:29 +0100

    add FA icons

--- packages/ssig-client/src/components/SignIn.js
+++ packages/ssig-client/src/components/SignIn.js
@@ -8,8 +8,13 @@ export default class SignIn extends Component {
   render() {
     return (
       <div>
+        <h3 className="title is-3">Sign in</h3>
+
         <button className="button" onClick={this.handleTwitterLoginClick}>
-          Twitter
+          <span className="icon">
+            <i className="fab fa-twitter" />
+          </span>
+          <span>Twitter</span>
         </button>
       </div>
     );
