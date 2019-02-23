b8b15acca5dc70bdaedeb1d31918dccb9de9ea1c
Michal Pasierbski <michal.pasierbski@r3pi.io>
2019-02-23 18:30:01 +0100

    database config env dependent

--- packages/ssig-server/config/database.js
+++ packages/ssig-server/config/database.js
@@ -1,18 +1,13 @@
+const defaultConfig = {
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  dialect: "mysql"
+};
+
 module.exports = {
-  "development": {
-    "username": "root",
-    "password": "changemessig",
-    "database": "ssig_development",
-    "host": "0.0.0.0",
-    "port": 3306,
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": process.env.DATABASE_USERNAME,
-    "password": process.env.DATABASE_PASSWORD,
-    "database": "ssig_production",
-    "host": process.env.DATABASE_HOST,
-    "port": process.env.DATABASE_PORT,
-    "dialect": "mysql"
-  }
-}
+  development: defaultConfig,
+  production: defaultConfig
+};
