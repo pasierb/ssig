ce01639f99962acf5749a8b5bf100e8bcc9e8530
Michal Pasierbski <michal.pasierbski@r3pi.io>
2019-03-01 08:19:28 +0100

    add text layer max line length control

--- packages/ssig-renderer/src/drawTextLayer.js
+++ packages/ssig-renderer/src/drawTextLayer.js
@@ -1,39 +1,43 @@
 import { setupCanvas, setShadow } from "./helpers";
 
 /**
- * 
- * @param {HTMLCanvasElement} canvas 
- * @param {object} layer 
+ *
+ * @param {HTMLCanvasElement} canvas
+ * @param {object} layer
  */
 export default function drawTextLayer(canvas, layer) {
-  const breakCharCount = 20;
-  const { x, y, typeData } = layer
+  // const maxLineLength = 20;
+  const { x, y, typeData } = layer;
   let { lineHeight } = typeData;
-  const { fontSize = "14", fontFamily, color = "#000", shadow, text = "" } = typeData;
+  const {
+    fontSize = "14",
+    fontFamily,
+    color = "#000",
+    shadow,
+    text = "",
+    maxLineLength = Infinity
+  } = typeData;
 
   if (!lineHeight) {
     lineHeight = fontSize;
   }
 
-  const rows = text.split(/\s/).reduce(
-    (acc, word) => {
-      const row = acc.pop() || "";
+  const rows = text.split(/\s/).reduce((acc, word) => {
+    const row = acc.pop() || "";
 
-      if (!row) {
-        acc.push(word);
-        return acc;
-      }
-
-      if (row.length + word.length > breakCharCount) {
-        acc.push(row, word);
-      } else {
-        acc.push([row, word].join(" "));
-      }
-
+    if (!row) {
+      acc.push(word);
       return acc;
-    },
-    []
-  );
+    }
+
+    if (row.length + word.length > maxLineLength) {
+      acc.push(row, word);
+    } else {
+      acc.push([row, word].join(" "));
+    }
+
+    return acc;
+  }, []);
 
   setupCanvas(canvas, ctx => {
     if (shadow) {
