ae2e652faa2d87f9d24338e548048e70aa107d05
Michal Pasierbski <michal.pasierbski@r3pi.io>
2019-03-26 13:19:06 +0100

    animate editor layer panels

--- packages/ssig-client/src/components/LayerList/LayerPanel.js
+++ packages/ssig-client/src/components/LayerList/LayerPanel.js
@@ -1,4 +1,4 @@
-import { h } from "preact";
+import { h, Component } from "preact";
 import styles from "./LayerPanel.scss";
 
 const LAYER_TYPE_ICON_CLASS = {
@@ -7,39 +7,117 @@ const LAYER_TYPE_ICON_CLASS = {
   rectangular: "fas fa-square"
 };
 
-export default function LayerPanel(props) {
-  const { layer, onClick, isActive, children } = props;
+// credit: https://css-tricks.com/using-css-transitions-auto-dimensions/
+function collapseSection(element) {
+  const sectionHeight = element.scrollHeight;
+  const elementTransition = element.style.transition;
 
-  const handleClick = e => {
-    e.preventDefault();
-    onClick(layer);
+  element.style.transition = "";
+
+  requestAnimationFrame(function() {
+    element.style.height = sectionHeight + "px";
+    element.style.transition = elementTransition;
+
+    requestAnimationFrame(function() {
+      element.style.height = 0 + "px";
+    });
+  });
+}
+
+function expandSection(element) {
+  const sectionHeight = element.scrollHeight;
+
+  const transitionendListener = () => {
+    element.removeEventListener("transitionend", transitionendListener);
+
+    element.style.height = null;
   };
 
-  const titleClass = [
-    "panel-block",
-    styles["LayerPanel__header"],
-    isActive ? styles["LayerPanel__header--is-active"] : null,
-  ]
-    .filter(i => i)
-    .join(" ");
+  element.style.height = sectionHeight + "px";
+  element.addEventListener("transitioned", transitionendListener);
+}
 
-  const contentClass = [
-    "panel-block",
-    styles.LayerPanel__content,
-    isActive ? styles["LayerPanel__content--active"] : null
-  ]
-    .filter(i => i)
-    .join(" ");
+export default class LayerPanel extends Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <nav className={["panel", styles.LayerPanel].join(" ")}>
-      <a className={titleClass} onClick={handleClick}>
-        <span class="panel-icon">
-          <i class={LAYER_TYPE_ICON_CLASS[layer.type]} aria-hidden="true" />
-        </span>
-        {layer.name}
-      </a>
-      <div className={contentClass}>{children}</div>
-    </nav>
-  );
+    this.state = {
+      isActive: false
+    };
+    this.contentRef = null;
+  }
+
+  handleClick = e => {
+    e.preventDefault();
+
+    this.toggle(() => this.props.onClick(this.props.layer));
+    // this.setState(
+    //   state => ({
+    //     isActive: !state.isActive
+    //   }),
+    //   () => {
+    //     this.props.onClick(this.props.layer);
+    //   }
+    // );
+  };
+
+  expand = () => expandSection(this.contentRef);
+
+  collapse = () => collapseSection(this.contentRef);
+
+  toggle = cb => {
+    this.setState(
+      state => ({
+        isActive: !state.isActive
+      }),
+      () => {
+        if (this.state.isActive) {
+          this.expand();
+        } else {
+          this.collapse();
+        }
+
+        cb && cb(this.state.isActive);
+      }
+    );
+  };
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.isActive !== this.state.isActive) {
+      this.toggle();
+    }
+  }
+
+  render(props) {
+    const { layer, isActive, children } = props;
+
+    const titleClass = [
+      "panel-block",
+      styles["LayerPanel__header"],
+      isActive ? styles["LayerPanel__header--is-active"] : null
+    ]
+      .filter(i => i)
+      .join(" ");
+
+    const contentClass = [
+      styles.LayerPanel__content,
+      isActive ? styles["LayerPanel__content--active"] : null
+    ]
+      .filter(i => i)
+      .join(" ");
+
+    return (
+      <nav className={["panel", styles.LayerPanel].join(" ")}>
+        <a className={titleClass} onClick={this.handleClick}>
+          <span class="panel-icon">
+            <i class={LAYER_TYPE_ICON_CLASS[layer.type]} aria-hidden="true" />
+          </span>
+          {layer.name}
+        </a>
+        <div className={contentClass} ref={el => (this.contentRef = el)}>
+          <div className="panel-block">{children}</div>
+        </div>
+      </nav>
+    );
+  }
 }
